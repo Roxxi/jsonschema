8d211496c581c45e85b14a62d347197c8b6bd8af
Matt Halverson <mhalverson@onekingslane.com>
2013-11-19 20:38:51 -0800

    Update clojure-common-utils version

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-
 (defproject roxxi/jsonschema "0.1.17"
   :description "Library to help you derive schemas from arbirary collections of JSON"
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [roxxi/clojure-common-utils "0.0.5"]
+                 [roxxi/clojure-common-utils "0.0.18"]
                  [cheshire "4.0.3"]
                  [roxxi/simple-avro "0.0.6"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
