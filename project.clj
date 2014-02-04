849f5890649cc584ef99d2a185d3e676a1280b58
Matt Halverson <mhalverson@onekingslane.com>
2014-02-04 12:03:45 -0800

    Update clojure-common-utils version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [roxxi/clojure-common-utils "0.0.18"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.19"]
                  [cheshire "4.0.3"]
                  [roxxi/simple-avro "0.0.6"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
