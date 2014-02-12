a2f5b3f8985595e88209944fae9115a344207cda
Shawn <sshah@onekingslane.com>
2014-02-12 12:48:25 -0800

    sql server type mappings

---
+++ src/jsonschema/type_system/db_types/sql_server.clj
@@ -0,0 +1,337 @@
+(ns jsonschema.type-system.db-types.sql-server
+  "Type translation from database column types to JSON schema types"
+  {:author "Shawn Shah"
+   :date "2/10/2014"}
+  (:require [clojure.string :as string]
+            [clojure.math.numeric-tower :as math]
+            [roxxi.utils.print :refer [print-expr]]
+            [jsonschema.type-system.types :as json-types]
+            [slingshot.slingshot :as slingshot]))
+
+;; SQL Server (T-SQL) Type Conversions
+;; Some forward declarations
+(declare col-def-str->col-map)
+(declare col-def-str->type-str)
+(declare col-def-str->length-str)
+(declare col-def-str->col-type-length)
+(declare col-def-str-is-unsigned?)
+(declare min-or-default-num)
+(declare str-or-nil->int-or-nil)
+(declare coalesce-with-limit)
+
+;; type mapping
+(def col-type->json-type-kw
+  {
+   "tinyint" :int
+   "smallint" :int
+   "int" :int
+   "bigint" :int
+   "rowversion" :int
+   "timestamp" :int
+   "real" :real
+   "float" :real
+   "decimal" :real
+   "money" :real
+   "smallmoney" :real
+   "date" :date
+   "datetime" :date
+   "datetime2" :date
+   "smalldatetime" :date
+   "datetimeoffset" :date
+   "time" :date
+   "char" :str
+   "varchar" :str
+   "nchar" :str
+   "nvarchar" :str
+   "binary" :str
+   "varbinary" :str
+   "text" :str
+   "ntext" :str
+   "image" :str
+   "uniqueidentifier" :str
+   "xml" :str
+ })
+
+(defmulti col-map->json-type
+  "Multimethod. Takes a map with column attributes
+like {:json-type :int :mysql-type-kw :int_unsigned :col-length 10}"
+  (fn [col-map]
+    (:json-type col-map)))
+
+;;;;;;;;;;;;;;;; INTEGER TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; SQL-Server Integer Type definitions
+;; (see: http://msdn.microsoft.com/en-us/library/ms187745.aspx)
+
+;; INT
+;; BIGINT
+;; SMALLINT
+;; TINYINT
+
+;; ROWVERSION/TIMESTAMP
+;; (see: http://msdn.microsoft.com/en-us/library/ms182776.aspx)
+
+(def int-synonym->int-type-kw
+  {
+   :rowversion :int
+   :timestamp :int
+   })
+
+(defn- translate-int-type [sqls-type-kw]
+  "If binary type is a synonym, translate to canonical type.
+Otherwise, pass type argument through."
+  (if (contains? int-synonym->int-type-kw sqls-type-kw)
+    (get int-synonym->int-type-kw sqls-type-kw)
+    sqls-type-kw))
+
+(def int-type-kw->min-max
+  {
+   :tinyint {:min 0 :max 255}
+   :smallint {:min (math/expt -2 15) :max (- (math/expt 2 15) 1)}
+   :int {:min (math/expt -2 31) :max (- (math/expt 2 31) 1)}
+   :bigint {:min (math/expt -2 63) :max (- (math/expt 2 63) 1)}
+   })
+
+(defmethod col-map->json-type :int [col-map]
+  (let [translated-col-type (translate-int-type (:sqls-type-kw col-map))
+        min-max (translated-col-type int-type-kw->min-max)]
+    (json-types/make-int (:min min-max) (:max min-max))))
+
+;;;;;;;;;;;;;;;; STRING & BINARY TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (see: http://msdn.microsoft.com/en-us/library/ms176089.aspx)
+;; CHARACTER
+;; CHAR
+;; VARCHAR
+;; NCHAR
+;; NVARCHAR
+;; BINARY
+;; VARBINARY
+
+;; IMAGE (deprecated)
+;; NTEXT (deprecated)
+;; TEXT (deprecated)
+
+;; UNIQUEIDENTIFIER
+;; (see: http://msdn.microsoft.com/en-us/library/ms187942.aspx)
+
+;; XML
+;; (see: http://msdn.microsoft.com/en-us/library/ms187339.aspx)
+
+(def str-synonym->str-type-kw
+  {
+   :character :varchar
+   :char :varchar
+   :nchar :varchar
+   :nvarchar :varchar
+   :binary :varchar
+   :varbinary :varchar
+   :uniqueidentifier :varchar
+   :image :text
+   :xml :text
+   })
+
+(def str-type-kw->default-min-max-length
+  {
+   :varchar {:default 1 :min 1 :max 8000}
+   :text (zipmap [:default :min :max] (repeat 3 (- (math/expt 2 31) 1)))
+   :ntext (zipmap [:default :min :max] (repeat 3 (- (math/expt 2 30) 1)))
+   })
+
+(defmulti str-col-map->default-min-max
+  (fn [x]
+    (:sqls-type-kw x)))
+
+;; Multimethods for column parameter handling.
+;; Column parameters, if present, are hints to modify the
+;; default limits of the column. For example, the n parameter
+;; to varchar(n) will extend the default maximum length to n.
+;; n can also be an arbitrary string like "max", so a global
+;; assumption about the type of column parameters cannot be made.
+;; As such, a dispatch method may exist for each type to do
+;; special handling of the column parameters and modify
+;; the column limits. Otherwise, the default column limits will pass through.
+
+
+(defn read-string-or-nil [val]
+  (if (nil? val)
+    nil
+    (read-string val)))
+
+(defmethod str-col-map->default-min-max :varchar [col-map]
+  "Special handling for varchar length parameter"
+  (let [default-min-max (str-type-kw->default-min-max-length
+                         (:sqls-type-kw col-map))
+        col-param-or-nil (read-string-or-nil (:col-length col-map))]
+    (condp = col-param-or-nil
+      (symbol "max") (assoc default-min-max :max (- (math/expt 2 31) 1))
+       nil default-min-max
+       (assoc default-min-max :max (coalesce-with-limit
+                                    col-param-or-nil
+                                    (:default default-min-max)
+                                    (:max default-min-max))))))
+
+(defmethod str-col-map->default-min-max :default [col-map]
+  "Special handling for varchar length parameter"
+  (let [default-min-max (str-type-kw->default-min-max-length
+                         (:sqls-type-kw col-map))]
+    default-min-max))
+
+(defmethod str-col-map->default-min-max :default [col-map]
+  "Default behavior, treat column length as integer, apply to column max"
+  (let [default-min-max (str-type-kw->default-min-max-length
+                         (:sqls-type-kw col-map))
+        n-col-param (str-or-nil->int-or-nil (:col-length col-map))
+        coalesced-max (coalesce-with-limit
+                       n-col-param
+                       (:default default-min-max)
+                       (:max default-min-max))]
+    {:min 1 :max coalesced-max}))
+
+(defn- translate-str-type [sqls-type-kw]
+  "If binary type is a synonym, translate to canonical type.
+Otherwise, pass binary type through."
+  (if (contains? str-synonym->str-type-kw sqls-type-kw)
+    (get str-synonym->str-type-kw sqls-type-kw)
+    sqls-type-kw))
+
+(defmethod col-map->json-type :str [col-map]
+  (let [str-type-kw (translate-str-type (:sqls-type-kw col-map))
+        translated-col-map (assoc col-map :sqls-type-kw str-type-kw)
+        col-limits (str-col-map->default-min-max translated-col-map)]
+    (json-types/make-str (:min col-limits) (:max col-limits))))
+
+;;;;;;;;;;;;;;;; DATE TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Date & Time (http://msdn.microsoft.com/en-us/library/ff848733.aspx)
+;; DATE
+;; DATETIME
+;; DATETIME2
+;; DATETIMEOFFSET
+;; SMALLDATETIME
+
+;; Note: TIME has a slightly different date format (number of microseconds shown)
+;; based on number of bytes requested
+;; TIME
+
+(def date-synonym->date-type-kw {})
+
+(def date-type-kw->date-format-patterns
+  {
+   :date ["YYYY-MM-DD"]
+   :datetime ["yyyy-MM-dd HH:mm:ss"]
+   :datetime2 ["yyyy-MM-dd HH:mm:ss.SSSSSSS"]
+   :datetimeoffset ["yyyy-MM-dd HH:mm:ss.SSSSSSSXXX"]
+   :smalldatetime ["yyyy-MM-dd HH:mm:00"]
+   :time ["HH:mm:ss.SSSSSSS"]
+   })
+
+;; TODO: move all these translation behavior into
+;; a more type-agnostic function
+(defn- translate-date-type [sqls-type-kw]
+  "If date type is a synonym, translate to canonical type.
+Otherwise, pass date type through."
+  (if (contains? date-synonym->date-type-kw sqls-type-kw)
+    (get date-synonym->date-type-kw sqls-type-kw)
+    sqls-type-kw))
+
+(defmethod col-map->json-type :date [col-map]
+  (let [date-type-kw (translate-date-type  (:sqls-type-kw col-map))
+        date-format-patterns (date-type-kw date-type-kw->date-format-patterns)]
+    (json-types/make-date date-format-patterns)))
+
+;;;;;;;;;;;;;;;; REAL TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Approximates
+;; (see: http://msdn.microsoft.com/en-us/library/ms173773.aspx)
+;; FLOAT
+;; REAL
+
+;; Decimal & Numeric
+;; (see: http://msdn.microsoft.com/en-us/library/ms187746.aspx)
+;; DECIMAL
+;; NUMERIC
+
+;; Money & Smallmoney
+;; (see: http://msdn.microsoft.com/en-us/library/ms179882.aspx)
+;; MONEY
+;; SMALLMONEY
+
+(def real-type-kw->min-max
+  {
+   :float {:min  -1.79E308 :max 1.79E308}
+   :real {:min -3.40E38 :max  3.40E38}
+   :decimal {:min (+ (math/expt -10 38) 1) :max (- (math/expt 10 38) 1)}
+   :money {:min -922337203685477.5808 :max 922337203685477.5807}
+   :smallmoney {:min -214748.3648 :max 214748.3647}
+   })
+
+;; TODO clean this up to avoid repeated typing of the same kws
+;; maybe something like:
+;;    :double [:float :float8 :real]
+;;    :numeric [:decimal :number :money :interval]
+(def real-synonym->real-type-kw
+  {
+   :numeric :decimal
+   })
+
+;; TODO: refactor duplicated logic with other translate-*-type functions
+(defn- translate-real-type [sqls-type-kw]
+  "If real type is a synonym, translate to canonical type.
+Otherwise, pass real type through."
+  (if (contains? real-synonym->real-type-kw sqls-type-kw)
+    (get real-synonym->real-type-kw sqls-type-kw)
+    sqls-type-kw))
+
+(defmethod col-map->json-type :real [col-map]
+  (let [real-type-kw (translate-real-type (:sqls-type-kw col-map))
+        min-max (real-type-kw real-type-kw->min-max)]
+    (json-types/make-real (:min min-max) (:max min-max))))
+
+;;;;;;;;;;;;;;;;;;;;;;;; UNIMPLEMENTED "OTHER" TYPES ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CURSOR
+;; HIERARCHYID
+;; SQLVARIANT
+;; GEOGRAPHY
+;; GEOMETRY
+
+;; Common functions
+;; TODO: Refactor. Lots of duplication here with mysql.clj common functions
+(defn- str-or-nil->int-or-nil [str-or-nil]
+   (if (nil? str-or-nil) nil
+       (Integer. str-or-nil)))
+
+(defn- coalesce-with-limit [n-val n-default-val n-max-val]
+  "If n-val is nil, return the n-default-val. If val is NOT nil,
+return the minimum of n-val and n-max-val"
+  (if (nil? n-val) n-default-val (min n-val n-max-val)))
+
+(defn- col-def-str-is-unsigned? [^String col-def-str]
+  (let [str-parts (string/split (string/lower-case col-def-str) #"[\s]+")]
+    (and (= 2 (count str-parts)) (= (second str-parts) "unsigned"))))
+
+(defn- col-def-str->type-str [col-def-str]
+  (first (string/split col-def-str #"[^\w]+")))
+
+(defn- col-def-str->length-str [col-def-str]
+  (let [matches (re-find #"\(([^\)]+)\)" col-def-str)]
+    (if (> (count matches) 1) (second matches) nil)))
+
+(defn- col-def-str->col-type-length [^String col-def-str]
+  (let [col-type-str (col-def-str->type-str col-def-str)
+        col-length-str (col-def-str->length-str col-def-str)]
+    {:col-type col-type-str :col-length col-length-str}))
+
+(defn- col-def-str->col-map [^String col-def-str]
+  "Transform a column definition (i.e. 'int(11)') into map with column attributes"
+  (let [col-type-length (col-def-str->col-type-length col-def-str)
+        col-json-type (col-type->json-type-kw (:col-type col-type-length))
+        sqls-type-kw (keyword (col-def-str->type-str col-def-str))]
+     {:json-type col-json-type
+      :sqls-type-kw sqls-type-kw
+      :col-length (:col-length col-type-length)}))
+
+(defn col-type->json-type [^String col-def-str]
+  "Transform a mysql type string (i.e. 'int(10) unsigned') into a JSONSchema type"
+  (let [col-map (col-def-str->col-map col-def-str)]
+    (col-map->json-type col-map)))
---
+++ test/jsonschema/sql_server_types_test.clj
@@ -0,0 +1,298 @@
+(ns jsonschema.sql-server-types-test
+  (:use clojure.test
+        roxxi.utils.print
+        jsonschema.transform)
+  (:require [clojure.math.numeric-tower :as math]
+            [jsonschema.type-system.types :as types]
+            [jsonschema.type-system.db-types.sql-server :as sqls-types]))
+
+;;;;;;;;;;;;;;;; INTEGER TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (see: http://msdn.microsoft.com/en-us/library/ms187745.aspx)
+
+;; INT
+;; BIGINT
+;; SMALLINT
+;; TINYINT
+
+(deftest sqls-tinyint->json-type-test []
+  (let [int-type (sqls-types/col-type->json-type "tinyint")]
+    (is (= (type int-type) jsonschema.type_system.types.Int))
+    (is (= (types/getMin int-type) 0))
+    (is (= (types/getMax int-type) 255))))
+
+(deftest sqls-smallint->json-type-test []
+  (let [int-type (sqls-types/col-type->json-type "smallint")]
+    (is (= (type int-type) jsonschema.type_system.types.Int))
+    (is (= (types/getMin int-type) (math/expt -2 15)))
+    (is (= (types/getMax int-type) (- (math/expt 2 15) 1)))))
+
+(deftest sqls-int->json-type-test []
+  (let [int-type (sqls-types/col-type->json-type "int")]
+    (is (= (type int-type) jsonschema.type_system.types.Int))
+    (is (= (types/getMin int-type) (math/expt -2 31)))
+    (is (= (types/getMax int-type) (- (math/expt 2 31) 1)))))
+
+(deftest sqls-bigint->json-type-test []
+  (let [int-type (sqls-types/col-type->json-type "bigint")]
+    (is (= (type int-type) jsonschema.type_system.types.Int))
+    (is (= (types/getMin int-type) (math/expt -2 63)))
+    (is (= (types/getMax int-type) (- (math/expt 2 63) 1)))))
+
+;;;;;;;;;;;;;;;; NUMERICS ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (see: http://msdn.microsoft.com/en-us/library/ms173773.aspx)
+;; FLOAT
+;; REAL
+
+(deftest sqls-float->json-type-test []
+  (let [num-type (sqls-types/col-type->json-type "float")]
+    (is (= (type num-type) jsonschema.type_system.types.Real))
+    (is (= (types/getMin num-type) -1.79E308))
+    (is (= (types/getMax num-type) 1.79E308))))
+
+(deftest sqls-real->json-type-test []
+  (let [num-type (sqls-types/col-type->json-type "real")]
+    (is (= (type num-type) jsonschema.type_system.types.Real))
+    (is (= (types/getMin num-type) -3.40E38))
+    (is (= (types/getMax num-type) 3.40E38))))
+
+(deftest sqls-decimal->json-type-test []
+  (let [num-type (sqls-types/col-type->json-type "decimal")]
+    (is (= (type num-type) jsonschema.type_system.types.Real))
+    (is (= (types/getMin num-type) (+ (math/expt -10 38) 1)))
+    (is (= (types/getMax num-type) (- (math/expt 10 38) 1)))))
+
+(deftest sqls-money->json-type-test []
+  (let [num-type (sqls-types/col-type->json-type "money")]
+    (is (= (type num-type) jsonschema.type_system.types.Real))
+    (is (= (types/getMin num-type) -922337203685477.5808))
+    (is (= (types/getMax num-type) 922337203685477.5807))))
+
+(deftest sqls-smallmoney->json-type-test []
+  (let [num-type (sqls-types/col-type->json-type "smallmoney")]
+    (is (= (type num-type) jsonschema.type_system.types.Real))
+    (is (= (types/getMin num-type) -214748.3648))
+    (is (= (types/getMax num-type) 214748.3647))))
+
+;;;;;;;;;;;;;;;;;;;;;;;; DATES ;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Date & Time (see: http://msdn.microsoft.com/en-us/library/ff848733.aspx)
+;; DATE
+;; DATETIME
+;; DATETIME2
+;; DATETIMEOFFSET
+;; SMALLDATETIME
+
+(deftest sqls-date->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "date")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"YYYY-MM-DD"}))))
+
+(deftest sqls-datetime->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "datetime")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"yyyy-MM-dd HH:mm:ss"}))))
+
+(deftest sqls-datetime2->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "datetime2")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"yyyy-MM-dd HH:mm:ss.SSSSSSS"}))))
+
+(deftest sqls-datetimeoffset->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "datetimeoffset")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"yyyy-MM-dd HH:mm:ss.SSSSSSSXXX"}))))
+
+(deftest sqls-smalldatetime->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "smalldatetime")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"yyyy-MM-dd HH:mm:00"}))))
+
+(deftest sqls-time->json-type-test []
+  (let [date-type (sqls-types/col-type->json-type "time")]
+    (is (= (type date-type) jsonschema.type_system.types.Date))
+    (is (= (types/getFormats date-type) #{"HH:mm:ss.SSSSSSS"}))))
+
+;;;;;;;;;;;;;;;; STRING & BINARY TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+;; (see: http://msdn.microsoft.com/en-us/library/ff848814.aspx)
+
+(def MAX_30_BIT_VALUE (- (math/expt 2 31) 1))
+(def MAX_29_BIT_VALUE (- (math/expt 2 30) 1))
+
+;; CHAR
+(deftest sqls-char->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "char")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-char10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "char(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-char9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "char(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+;; VARCHAR
+(deftest sqls-varchar->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varchar")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-varchar10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varchar(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-varchar9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varchar(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+
+(deftest sqls-varchar9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varchar(max)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) (- (math/expt 2 31) 1)))))
+
+;;NCHAR
+
+(deftest sqls-nchar->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nchar")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-nchar10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nchar(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-nchar9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nchar(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-ncharmax->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nchar(max)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_30_BIT_VALUE))))
+
+;; NVARCHAR
+
+(deftest sqls-nvarchar->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nvarchar")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-nvarchar10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nvarchar(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-nvarchar9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nvarchar(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-nvarcharmax->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "nvarchar(max)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_30_BIT_VALUE))))
+
+;;BINARY
+
+(deftest sqls-binary->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "binary")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-binary10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "binary(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-binary9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "binary(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+;; VARBINARY
+
+(deftest sqls-varbinary->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varbinary")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-varbinary10->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varbinary(10)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 10))))
+
+(deftest sqls-varbinary9999->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varbinary(9999)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) 8000))))
+
+(deftest sqls-varbinarymax->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "varbinary(max)")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_30_BIT_VALUE))))
+
+;; TEXT
+(deftest sqls-text->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "text")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_30_BIT_VALUE))))
+
+(deftest sqls-image->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "image")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_30_BIT_VALUE))))
+
+(deftest sqls-ntext->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "ntext")]
+    (is (= (type json-type) jsonschema.type_system.types.Str))
+    (is (= (types/getMin json-type) 1))
+    (is (= (types/getMax json-type) MAX_29_BIT_VALUE))))
+
+;;;;;;;;;;;;;;;; OTHER TYPES ;;;;;;;;;;;;;;;;;;;;;;;;
+;; (see: http://msdn.microsoft.com/en-us/library/ms187745.aspx)
+
+;; ROWVERSION
+
+(deftest sqls-rowversion->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "rowversion")]
+    (is (= (type json-type) jsonschema.type_system.types.Int))))
+
+;; TIMESTAMP
+
+(deftest sqls-timestamp->json-type-test []
+  (let [json-type (sqls-types/col-type->json-type "timestamp")]
+    (is (= (type json-type) jsonschema.type_system.types.Int))))
